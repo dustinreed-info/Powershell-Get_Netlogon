ae52c46c35c88c1e969093333b76b87aaa538d23
Whocares <dust.in@sbcglobal.net>
2019-06-24 17:51:57 -0500

    Refactored script

--- get-netlogon.ps1
+++ get-netlogon.ps1
@@ -1,9 +1,9 @@
 $result = "netlogon-combinedresult.csv"
 $temp = "c:\temp\test"
-remove-item $temp\$result
+remove-item $temp\$result -ErrorAction SilentlyContinue
 $server1 = ""
 $server2 = ""
-$paths = "\\$server1\C$\windows\debug\netlogon.log","\\$server2\C$\windows\debug\netlogon.log" #,"\\$server1\C$\windows\debug\netlogon.bak","\\$server2\C$\windows\debug\netlogon.bak"
+$paths = "C:\temp\netlogon.log" #,"\\$server1\C$\windows\debug\netlogon.bak","\\$server2\C$\windows\debug\netlogon.bak"
 $i = 1
 $to = "whoever@example.com"
 $from = "you@example.com"
@@ -16,64 +16,22 @@ foreach ($path in $paths) {
     import-csv $temp\netlogon$i.csv  | Export-Csv $temp\netlogon-combined.csv -Append -Force
     $i++
 } 
-$Csv = dir $temp\netlogon*.csv
+
+$text = [io.file]::readalltext("$temp\netlogon-combined.csv")
+$text.replace("[LOGON]","").replace("from",",").replace("SamLogon:",",").replace('"','')  | Out-File $temp\netlogon.csv
 
 
-[io.file]::readalltext("$temp\netlogon-combined.csv").replace("[LOGON]",",")  |  Out-File $temp\netlogon-1.csv
-[io.file]::readalltext("$temp\netlogon-1.csv").replace("from",",") |  Out-File $temp\netlogon.txt
-[io.file]::readalltext("$temp\netlogon.txt").replace("SamLogon:",",") |  Out-File $temp\netlogon21.txt
-[io.file]::readalltext("$temp\netlogon21.txt").replace('"','') |  Out-File $temp\netlogon.csv
-
-select-string "0xC0000064","0xC000006C","0xc000006D","0xC0000071","0xC0000072","0xC0000193","0xC0000224","0xC0000234","0xC000006A" -path "$temp\netlogon.csv" | `
+select-string "0xC0000064","0xC000006C","0xc000006D","0xC0000071","0xC0000072","0xC0000193","0xC0000224","0xC0000234","0xC000006A","0xC000005E" -path "$temp\netlogon.csv" | `
 select-string -Pattern "CRITICAL" -NotMatch | export-csv $temp\netlogon-2.csv -NoTypeInformation
 
-Import-Csv $temp\netlogon-2.csv | select -expandproperty line | Out-File $temp\netlogon-result.csv 
+Import-Csv $temp\netlogon-2.csv | Select-Object -expandproperty line | Out-File $temp\netlogon-result.csv 
 Import-Csv $temp\netlogon-result.csv |export-csv $temp\netlogon-combinedresult.csv -NoTypeInformation
 
-get-childitem $temp\*.csv | ? {$_.name -ne "netlogon-combinedresult.csv"} | remove-item 
-remove-item $temp\netlogon.txt  -Force
-
-
-#$B = Out-File $temp\netlogon-combinedresult.html 
+get-childitem $temp\netlogon*.csv | ? {$_.name -ne "netlogon-combinedresult.csv"} | remove-item 
+# remove-item $temp\netlogon.txt  -Force
 
 
 
-send-mailmessage -to "dreed@portfoliopathway.com" -from "dra@portfoliopathway.com" -SmtpServer "pp-prd-exc.portfoliopathway.com" -port 25250 `
--Subject "Portfolio Pathway Failed NetLogons" -Attachments $temp\netlogon-combinedresult.csv
+# send-mailmessage -to $to -from $from -SmtpServer $mail_server -port $port `
+# -Subject $subject -Attachments $temp\netlogon-combinedresult.csv
 
-$b = import-csv $temp\netlogon-combinedresult.csv | Out-String
-
-#if ($b -like "*ccmfile*") {
-    $temp = "c:\temp\test"
-    $paths = "\\storage.ccmg.com\C$\windows\debug\netlogon.log","\\ccm-vhost1.ccmg.com\C$\windows\debug\netlogon.log"#,"\\storage.ccmg.com\C$\windows\debug\netlogon.bak","\\ccm-vhost1.ccmg.com\C$\windows\debug\netlogon.bak"
-    $i = 1
-    
-    foreach ($path in $paths) {  
-        copy $path $temp\ccmg-netlogon$i.csv 
-        import-csv $temp\ccmg-netlogon$i.csv  | Out-File $temp\ccmg-netlogon-combined.csv -Append
-        $i++
-    } 
-    $Csv = dir $temp\ccmg-netlogon*.csv
-    
-    #foreach ($C in $CSV) { 
-    #import-csv $c  | Out-File $temp\netlogon-combined.csv -Append 
-    #}
-    [io.file]::readalltext("$temp\ccmg-netlogon-combined.csv").replace("[LOGON]",",")  |  Out-File $temp\ccmg-netlogon-1.csv
-    [io.file]::readalltext("$temp\ccmg-netlogon-1.csv").replace("from",",") |  Out-File $temp\ccmg-netlogon.txt
-    [io.file]::readalltext("$temp\ccmg-netlogon.txt").replace("SamLogon:",",") |  Out-File $temp\ccmg-netlogon21.txt
-    [io.file]::readalltext("$temp\ccmg-netlogon21.txt").replace('"','') |  Out-File $temp\ccmg-netlogon.csv
-    
-    select-string "0xC0000064","0xC000006C","0xc000006D","0xC0000071","0xC0000072","0xC0000193","0xC0000224","0xC0000234" -path "$temp\ccmg-netlogon.csv" | `
-    select-string -Pattern "CRITICAL" -NotMatch | export-csv $temp\ccmg-netlogon-2.csv -NoTypeInformation
-    
-    Import-Csv $temp\ccmg-netlogon-2.csv | select -expandproperty line | Out-File $temp\ccmg-netlogon-result.csv 
-    Import-Csv $temp\ccmg-netlogon-result.csv | export-csv $temp\ccmg-netlogon-combinedresult.csv -NoTypeInformation
-    
-   
-    remove-item $temp\ccmg-netlogon.txt  -Force
-    
-    
-send-mailmessage -to $to -from $from -SmtpServer $mail_server -port $port `
--Subject $subject -Attachments $temp\ccmg-netlogon-combinedresult.csv
-
-get-childitem $temp\*.csv | ? {$_.name -ne "netlogon-combinedresult.csv" -or "ccmg-netlogon-combinedresult.csv"} | remove-item 
